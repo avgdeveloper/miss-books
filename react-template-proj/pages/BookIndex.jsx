a629fd8bfeb252d69187cc2ba062291c108b6551
Aviad Gintzler <aviad@medisonmedia.com>
2026-04-29 19:19:46 +0300

    add book service with example data

--- react-template-proj/pages/BookIndex.jsx
+++ react-template-proj/pages/BookIndex.jsx
@@ -1,8 +1,14 @@
+import { bookService } from "../services/book.service.js"
 
-
-
+const { useEffect, useState } = React
 
 export function BookIndex() {
+
+    useEffect(() => {
+        bookService.query().then(books => {
+            return books;
+        })
+    }, [])
 
     return (
         <div>Book Index</div>
---
+++ react-template-proj/services/book.service.js
@@ -0,0 +1,54 @@
+import { storageService } from "./storage.service.js"
+const BOOKS_KEY = "books"
+_createBooks()
+
+export const bookService = {
+    query
+}
+
+function query(filterBy = {}) {
+    return storageService.loadFromStorage(BOOKS_KEY)
+        .then((books) => {
+            if (filterBy.title) {
+                const regExp = new RegExp(filterBy.title, "i")
+                books = books.filter(book => regExp.test(book.title))
+            }
+            return books;
+        })
+}
+
+function _createBooks() {
+    const model = [{
+        "id": "OXeMG8wNskc",
+        "title": "metus hendrerit",
+        "description": "placerat nisi sodales suscipit tellus",
+        "thumbnail": "http://ca.org/books-photos/20.jpg",
+        "listPrice": {
+            "amount": 109,
+            "currencyCode": "EUR",
+            "isOnSale": false
+        }
+    }, {
+        "id": "OXeMG8we34",
+        "title": "lorem hendrerit",
+        "description": "placerat nisi sodales suscipit tellus",
+        "thumbnail": "http://ca.org/books-photos/20.jpg",
+        "listPrice": {
+            "amount": 109,
+            "currencyCode": "EUR",
+            "isOnSale": true
+        }
+    }, {
+        "id": "OXeMG8wN23df",
+        "title": "lorem ipsum",
+        "description": "placerat nisi sodales suscipit tellus",
+        "thumbnail": "http://ca.org/books-photos/20.jpg",
+        "listPrice": {
+            "amount": 109,
+            "currencyCode": "EUR",
+            "isOnSale": false
+        }
+    }];
+
+    storageService.saveToStorage("books", model);
+}
